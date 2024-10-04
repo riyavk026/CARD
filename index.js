891b96a28fa46d24edd947e449b1cb7cce00ad44
Riya <riyavk026@gmail.com>
2024-10-04 12:32:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+function createCard(title, cName, views, monthsOld, duration, thumbnail) {
+    // Finish this function
+    if views
+    let viewNumber =
+    let HTML = `<div class="card">
+    <div class="image">
+        <img src="https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw"
+            alt="">
+            <div class="capsule">31:06</div>
+    </div>
+    <div class="text">
+        <h1>${title} </h1>
+        <p> ${cName} .${viewNumber} views. ${title}    </p>
+    </div>`
+}
+
+    createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", 560000, 7, "31:22",    
+    "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")
